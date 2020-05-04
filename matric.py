62e711998fc8128f6c95b371feb790f530b8d7c5
hesam80 <hm.hoseyni@gmail.com>
2020-05-05 00:16:12 +0430

    fetch vs push pull

--- matric.py
+++ matric.py
@@ -1,8 +1,35 @@
 import numpy as np
-c=np.empty(8)
-print('matris c= ',c)
-d = np.empty([2, 2], dtype=int)
-print("matris d=", d)
-f=np.arange(5)
-print("matris f=", f)
+from pandas import  read_excel
+import random
+def matric():
+   c=np.empty(8)
+   print('matris c= ',c)
+   d = np.empty([2, 2], dtype=int)
+   print("matris d=", d)
+   f=np.arange(5)
+   print("matris f=", f)
 
+def tahlil():
+
+   df=read_excel('comptst.xlsx',0)
+   ind=df.index
+   decprition=df['dec']
+   part=df['pn']
+   serof=df['snf']
+   seron=df['snn']
+   #integrate=df['snf'].mean()
+   print(df.head())
+   #print("majmoo is:", integrate)
+
+def zoj(n):
+   num = random.randint(1,int(n))
+   for num in range(num):
+      if num % 2 == 0 :
+        print("found a zoj number :" , num)
+        continue
+      print("fard number: ",num)
+
+
+
+zoj(10.14)
+#tahlil()
