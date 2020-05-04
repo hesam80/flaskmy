4d36a07b18842a0b5c9685e45a1fff79c740cfe4
hesam80 <hm.hoseyni@gmail.com>
2020-05-04 14:05:38 +0430

    continue

--- matric.py
+++ matric.py
@@ -1,6 +1,6 @@
 import numpy as np
 from pandas import  read_excel
-
+import random
 def matric():
    c=np.empty(8)
    print('matris c= ',c)
@@ -21,4 +21,15 @@ def tahlil():
    print(df.head())
    #print("majmoo is:", integrate)
 
-tahlil()
+def zoj(n):
+   num = random.randint(1,int(n))
+   for num in range(num):
+      if num % 2 == 0 :
+        print("found a zoj number :" , num)
+        continue
+      print("fard number: ",num)
+
+
+
+zoj(10.14)
+#tahlil()
