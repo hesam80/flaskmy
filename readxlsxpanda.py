441c37026c4ea9f1abaa01968977c7e0e418cea7
hesam80 <hm.hoseyni@gmail.com>
2020-05-05 11:03:17 +0430

    numpy and pandas related together

--- readxlsxpanda.py
+++ readxlsxpanda.py
@@ -1,6 +1,9 @@
 from pandas import  read_excel
+import numpy as np
 df=read_excel('tst.xlsx',0)
-print(df)
-for index in df.itetrows():
-	print(index)
-	
+tst=df.head()
+sum_bedehkar=df['bedehkar'].sum()
+print(tst)
+print("sum_bedehkar is" ,sum_bedehkar)
+min_bedehkar=np.min(df['bedehkar'])
+print("min_bedehkar is ",min_bedehkar)
