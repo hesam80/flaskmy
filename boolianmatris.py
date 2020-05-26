f3aed607175b5a3a8950522158def247eca0949f
hesam80 <hm.hoseyni@gmail.com>
2020-05-26 23:53:04 +0430

    baraye neshan dadan dar browser

--- boolianmatris.py
+++ boolianmatris.py
@@ -1,10 +1,25 @@
+#!C:\xampp\htdocs\pythonpro\flaskmy\env\Scripts\python.exe
+#!C:\xampp\htdocs\pythonpro\flaskmy\matris.txt
 import stdio
 import stdarray
 import numpy as np
+
+print("Content-Type: text/html\n charset:utf-8\n")
+def hello(request):
+    return HttpResponse("Hello world")
+    print("Content-Type: text/html\n charset:utf-8\n")
+
+def readfile():
+	f= open('matris.txt')
+	content = f.read()
+	print(content)
+	return content
+
 def readb():
 	count = stdio.readInt()
 	print(count)
 	return count
+
 def readcell():
 	count = stdio.readInt()
 	a=stdarray.create1D(count, False)
@@ -31,12 +46,20 @@ def num_practice():
 	z[::2,1::2]=1
 	return z
 
+f=readfile()
+print(f)
+print("<hr>")
 x=num_practice()
 v=readcells()
 z=np.array(v)
 w=np.add(z,x)
 print(z)
+print("<br>")
 print(z.T)
+print("<br>")
 print(x)
+print("<br>")
 print(z.shape)
+print("<br>")
+print("<hr>",)
 print(w)
