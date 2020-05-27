d887145fef6945e8d27227ca961fdc996d555663
hesam80 <hm.hoseyni@gmail.com>
2020-05-27 12:37:04 +0430

    readbook az googlerads

--- readbooks.py
+++ readbooks.py
@@ -1,6 +1,6 @@
 import requests
 import bs4
-import urlib
+import urllib3 as urlib
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -9,19 +9,20 @@ def showbook(name):
 	try:
 		name=urlib.quote_plus(name)
 		baseURL='https://www.goodreads.com'
-		goodreads=requests.get
-(baseURL+"/search?utf8=%E2%9C%93&search_type=books&search%5Bfield=outhor&q="+name)
-		soap= bs4.BeautifulSoup(goodreads,content)
+		goodreads=requests.get(baseURL+"/search?utf8=%E2%9C%93&search_type=books&search%5Bfield=outhor&q="+name)
+		soap= bs4.BeautifulSoup(goodreads.content,"html.paresr")
 		frstbookURL=soap.find("a","bookTitle")['href']
-		print('"'+baseURL+"'")
+		print(baseURL)
 	except:
 		pass
+
 page= requests.get("https://en.m.wikipedia.org/wiki/List_of_authors_by_name:_M")
-soap=bs4.BeautifulSoup(page,content)
-names=soap.findALL("a")
+soap=bs4.BeautifulSoup(page.content,"html.parser")
+names=soap.findAll("a")
 
 for name in names:
-	if name.string is None:
+	if name is None:
+		print ("None")
 		continue
-	if name.string[0]=='J' and len(name.string)>2:
-		try:
+	if len(str(name.string))>2:
+		showbook(name)
