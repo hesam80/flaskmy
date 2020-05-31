7968a1652e6ad55397e406ae5c8e99571bd6a8c7
hesam80 <hm.hoseyni@gmail.com>
2020-05-31 14:43:51 +0430

    modify readop1 dtype Error!!!!!

--- readop1.py
+++ readop1.py
@@ -1,7 +1,11 @@
-from pandas import  read_excel
+#!C:\xampp\htdocs\pythonpro\flaskmy\env\Scripts\python.exe
+import pandas as pd
 import numpy as np
 def op_one():
-	df=read_excel('op1.xlsx',0)
+	df=pd.read_excel('op1.xlsx',0)
 	tst=df.head()
 	dp=df.groupby('Chapter').size()
 	print(dp)
+	print(df.dtype())
+
+op_one()
