a7e737f33b0fca5e01b8366e66ef7e38bb16ea1b
hesam80 <hm.hoseyni@gmail.com>
2020-06-01 23:37:30 +0430

    input

--- readop1.py
+++ readop1.py
@@ -1,5 +1,21 @@
 from pandas import  read_excel
 import numpy as np
+
+def arzyabi():
+	arz=[
+	'exercize',
+	'business',
+	'study',
+	'python',
+	]
+	answers=input(f"do you done"+ arz + "(y or n)")
+	count=0
+	for answer in answers:
+		if answer=='y':
+			count+=1
+	print(count)
+
+
 def op_one():
 	df=read_excel('op1.xlsx',0)
 	tst=df.head()
@@ -7,4 +23,5 @@ def op_one():
 	print(dp)
 	print(df.dtypes)
 
-op_one()
+#op_one()
+arzyabi()
