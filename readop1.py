c263fa04beb868e27b29c07d17a5e607466bb6de
hosseininasab hesam <hesam.hosseininasab@MahanAir.Aero>
2020-06-01 11:38:11 +0430

    tasks and input statement

--- readop1.py
+++ readop1.py
@@ -1,11 +1,32 @@
-#!C:\xampp\htdocs\pythonpro\flaskmy\env\Scripts\python.exe
-import pandas as pd
+from pandas import  read_excel
 import numpy as np
 def op_one():
-	df=pd.read_excel('op1.xlsx',0)
+	df=read_excel('op1.xlsx',0)
 	tst=df.head()
 	dp=df.groupby('Chapter').size()
 	print(dp)
-	print(df.dtype())
+	print(df.dtypes)
+	print(df.count())
+def task_to_do():
+	tasks=[
+	'sleep early in night',
+	'works regurallity',
+	'make a plan for days',
+	'exercize',
+	'review my weblog',
+	'study python',
+	]
+	answers=[]
+	rank=0
+	percentage=100/len(tasks)
+	for task in tasks:
+		answer=input("Did you"+task + "?"+"(y or n)")
+		answers.append(answer)
+		if answer=="y":
+			rank+=percentage
 
-op_one()
+	print("you've done"+ str(rank)+ "% of your tasks")
+
+
+task_to_do()
+#op_one()
