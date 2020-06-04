b5eae1bef4278e286d0abc712fde8ac1a42dcacf
hesam80 <hm.hoseyni@gmail.com>
2020-06-04 21:42:26 +0430

    modify readop1.py

--- readop1.py
+++ readop1.py
@@ -12,4 +12,21 @@ def op_one():
 	print(dp)
 	print(df.dtypes)
 
-op_one()
+def op1_analize(file):
+	df=read_excel('file',0)
+	return df['Chapter'].std()
+	tst1=df1.head()
+	print("sort date",date_sort)
+	#tst=df.head()
+	#dp=df.groupby('Chapter').size()
+	#print(dp)
+	#print(df.dtypes)
+	#print(df.count())
+def pandas_practce():
+	date=op_one()
+	print("sort",date)
+
+
+#op_one()
+#op1_analize(file)
+pandas_practce()
