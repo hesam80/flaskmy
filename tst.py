724d3a61d671ad955d1871a21a67b0744322c252
hesam80 <hm.hoseyni@gmail.com>
2023-12-05 05:48:45 +0000

    new modify

--- tst.py
+++ tst.py
@@ -15,8 +15,16 @@ def display_menu():
 
 def pandas_practice():
     df = pd.read_excel("comptst.xlsx")
+    df1 = pd.DataFrame({'a':[1,3,5,7,4,5,6,4,7,8,9],
+                   'b':[3,5,6,2,4,6,7,8,7,8,9]})
+    rows = df.index
+    description=df['DESCRIPTION']
+    partno=df['P/N']
+    print (description)
+    print(partno)
+    df2=pd.DataFrame({'DESCRIPTION':['MAIN WHEEL ASSY'],'P/N':['AHA1925-2']})
     print("column heading is:")
-    print(df.columns)
+    print(df2.columns)
 
 
 display_menu()
